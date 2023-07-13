44708f2ade8c8c0572a8fc7ae59bafc2b95e1e01
Aygün Bayır <turkoaygun@gmail.com>
2023-07-13 22:11:51 +0300

    express router & mvc

---
+++ routes/blogRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+  blog_index,
+  blog_create_post,
+  blog_create_get,
+  blog_details,
+  blog_delete,
+} = require('../controllers/blogController');
+
+const router = express.Router();
+
+router.get('/', blog_index);
+router.post('/', blog_create_post);
+router.get('/create', blog_create_get);
+router.get('/:id', blog_details);
+router.delete('/:id', blog_delete);
+
+module.exports = router;
